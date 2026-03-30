d483e028ae9cba66a7bd9adba06f18429a0f3132
EllieFroggie <ellie.lewis403@proton.me>
2026-03-30 10:47:11 -0600

    update build.sh to not suck

--- internal/providers/mpd/build.sh
+++ internal/providers/mpd/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-export GOTOOLCHAIN=go1.26.1
-export GOEXPERIMENT=nodwarf5
-export CGO_ENABLED=1
-export GOAMD64=v1
-go build -buildmode=plugin -ldflags="-s -w"  -trimpath -o mpd.so .
+go build -buildvcs=false -buildmode=plugin -trimpath -o mpd.so .
 
 rm $HOME/.config/elephant/providers/mpd.so
 mv mpd.so $HOME/.config/elephant/providers/
